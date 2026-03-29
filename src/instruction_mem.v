b4e9c2a4912d4975c009773133819b9ccabb4bed
Doruk <ethernit@Doruk-MacBook-Pro.local>
2026-03-29 23:46:38 +0300

    Fix: program.mem path for synthesis

--- src/instruction_mem.v
+++ src/instruction_mem.v
@@ -6,7 +6,7 @@ module instruction_mem (
     reg [15:0] rom [0:255];
 
     initial begin
-        $readmemb("programs/program.mem", rom);
+        $readmemb("program.mem", rom);
     end
 
     assign instruction = rom[addr];
